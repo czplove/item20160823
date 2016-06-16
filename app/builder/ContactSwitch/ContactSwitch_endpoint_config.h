9dcae4e4a6076c9ad9039ce59c84cc09e296c099
yuzz <zhengzheng.yu@wuliangroup.com>
2016-06-16 14:34:10 +0800

    Squashed commit of the following:
    commit 24df783676d78ef951fd6ed53d25119cb0438250
    Author: yuzz <zhengzheng.yu@wuliangroup.com>
    Date:   Thu Jun 16 14:26:17 2016 +0800
    
        修改认证产品的应用版本号为02
    
        Signed-off-by: yuzz <zhengzheng.yu@wuliangroup.com>

--- app/builder/ContactSwitch/ContactSwitch_endpoint_config.h
+++ app/builder/ContactSwitch/ContactSwitch_endpoint_config.h
@@ -16,7 +16,7 @@
 #define GENERATED_DEFAULTS { \
 6,'W','u','l','i','a','n',0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 /* 0,Default value: Basic,manufacturer name */, \
 6,'T','-','Y','G','0','1',0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 /* 33,Default value: Basic,model identifier */, \
-8,'2','0','1','6','0','4','0','9',0,0,0,0,0,0,0,0 /* 66,Default value: Basic,date code */, \
+8,'2','0','1','6','0','6','1','6',0,0,0,0,0,0,0,0 /* 66,Default value: Basic,date code */, \
 0x00, 0x00, 0x38, 0x40 /* 83,Default value: Poll Control,check-in interval */, \
 0x00, 0x00, 0x00, 0x50 /* 87,Default value: Poll Control,long poll interval */, \
   }
@@ -24,7 +24,7 @@
 #define GENERATED_DEFAULTS { \
 6,'W','u','l','i','a','n',0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 /* 0,Default value: Basic,manufacturer name */, \
 6,'T','-','Y','G','0','1',0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 /* 33,Default value: Basic,model identifier */, \
-8,'2','0','1','6','0','4','0','9',0,0,0,0,0,0,0,0 /* 66,Default value: Basic,date code */, \
+8,'2','0','1','6','0','6','1','6',0,0,0,0,0,0,0,0 /* 66,Default value: Basic,date code */, \
 0x40, 0x38, 0x00, 0x00 /* 83,Default value: Poll Control,check-in interval */, \
 0x50, 0x00, 0x00, 0x00 /* 87,Default value: Poll Control,long poll interval */, \
   }
@@ -36,7 +36,7 @@
 // Generated attributes
 #define GENERATED_ATTRIBUTES { \
     { 0x0000, ZCL_INT8U_ATTRIBUTE_TYPE, 1, (ATTRIBUTE_MASK_SINGLETON), { (uint8_t*)0x02 } }, /* 0 / Basic / ZCL version*/\
-    { 0x0001, ZCL_INT8U_ATTRIBUTE_TYPE, 1, (ATTRIBUTE_MASK_SINGLETON), { (uint8_t*)0x01 } }, /* 1 / Basic / application version*/\
+    { 0x0001, ZCL_INT8U_ATTRIBUTE_TYPE, 1, (ATTRIBUTE_MASK_SINGLETON), { (uint8_t*)0x02 } }, /* 1 / Basic / application version*/\
     { 0x0002, ZCL_INT8U_ATTRIBUTE_TYPE, 1, (ATTRIBUTE_MASK_SINGLETON), { (uint8_t*)0x01 } }, /* 2 / Basic / stack version*/\
     { 0x0003, ZCL_INT8U_ATTRIBUTE_TYPE, 1, (ATTRIBUTE_MASK_SINGLETON), { (uint8_t*)0x01 } }, /* 3 / Basic / hardware version*/\
     { 0x0004, ZCL_CHAR_STRING_ATTRIBUTE_TYPE, 33, (ATTRIBUTE_MASK_SINGLETON), { (uint8_t*)&(generatedDefaults[0]) } }, /* 4 / Basic / manufacturer name*/\
